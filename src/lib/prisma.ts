c8699395fa3298ceef2f9d879de84792b1f3b441
vatsal <vatsalsinghkv@gmail.com>
2026-08-25 13:39:27 +0530

    feat: dev tools add

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
+import { env } from "./env";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
+const connectionString = env.DATABASE_URL;
 const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
 const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
