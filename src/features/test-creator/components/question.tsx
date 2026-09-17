ee7043caddff84b1611a8fb959f895e875ac6aeb
vatsal <vatsalsinghkv@gmail.com>
2026-09-17 22:42:46 +0530

    feat: temp ui add for review

---
+++ src/features/test-creator/components/question.tsx
@@ -0,0 +1,7 @@
+import type { ParsedQuestion } from "../lib/types/parser";
+
+type Props = { question: ParsedQuestion };
+
+export default function Question({ question }: Props) {
+  return <div>Question</div>;
+}
--- src/features/test-creator/views/test-creator-view.tsx
+++ src/features/test-creator/views/test-creator-view.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-
 import { PageHeader } from "@/components/shared/page-header";
-
+import Review from "../components/steps/review";
 import Upload from "../components/steps/upload";
 import type { ParsedTest } from "../lib/types";
 
@@ -9,12 +8,12 @@ export function TestCreatorView() {
   const [parsedTest, setParsedTest] = useState<ParsedTest | null>(null);
 
   return (
-    <div className="relative">
+    <div className="relative h-full flex flex-col">
       <PageHeader name="Create New Test" />
 
-      <div className="p-5">
+      <div className="p-5 flex-1 flex flex-col min-h-0">
         {parsedTest ? (
-          <>Review</>
+          <Review data={parsedTest} onBack={() => setParsedTest(null)} />
         ) : (
           <Upload
             onParsed={(data) => {
